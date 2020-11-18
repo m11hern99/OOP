a40f4dba0f5626188142874db3939896cf1af9fb
Maria Hernandez <66804010+m11hern99@users.noreply.github.com>
2020-11-17 22:51:03 -0500

    Two track update

--- Assignment4/CircularTracksShape.java
+++ Assignment4/CircularTracksShape.java
@@ -12,14 +12,21 @@ public class CircularTracksShape implements Moveable
   private int width;
   int n;
   double theta = 0;
+  Color temp;
+  Color temp2;
+  Color temp3;
 
   private  double[] x = new double[20]; 
   private  double[] y = new double[20];
   
-  double[] xmax = new double[20];
-  double[] ymax = new double[20];
-  double[] xmin = new double[20];
-  double[] ymin = new double[20];
+  double x2;
+  double y2;
+  
+  double x3;
+  double y3;
+  
+  double x4;
+  double y4;
 
  Color[] cc = {Color.decode("#ADFFFA"), Color.decode("#FFADE1"), Color.decode("#F2FFAD"), Color.decode("#FFE0AD"),
                Color.decode("#ADB4FF"), Color.decode("#ADFFB0"), Color.decode("#FFA998"), Color.decode("#A9FA6D"),
@@ -33,9 +40,19 @@ public class CircularTracksShape implements Moveable
 	  n = width/40; 
 
 	  for(int i=0; i<= n-1; i=i+1){ 
-	   x[i] = 300 + (20 * i ) * Math.cos(theta)+10;
-	   y[i]=  300 + (20 * i) * Math.sin(theta);
-	 }
+	   x[i] = 300 + (20 * i + 10) * Math.cos(theta);
+	   y[i] = 300 + (20 * i + 10) * Math.sin(theta);
+	   
+	   x2 = 300 + 30 * Math.cos(theta + 3 * ( -Math.PI / Math.asin(.290002))); //I found that multiplying by 3 instead of 2 gave me a better placement.
+	   y2 = 300 + 30 * Math.sin(theta + 3 * ( -Math.PI / Math.asin(.290002)));
+	   
+	   x3 = 300 + 110 * Math.cos(theta + 3 * ( -Math.PI / Math.asin(.10002)));
+	   y3 = 300 + 110 * Math.sin(theta + 3 * ( -Math.PI / Math.asin(.10002)));
+	   
+	    x4 = 300 + 290 * Math.cos(theta + 3 * ( Math.PI / Math.asin(.030002)));
+	    y4 = 300 + 290 * Math.sin(theta + 3 * ( Math.PI / Math.asin(.030002)));
+
+	   }
 	  
 	
 }
@@ -50,8 +67,16 @@ public void translate(int dx, int dy){
 		int j=i+5;
 		x[i] = 300 + (((20*i+10)) * Math.cos((j*-theta)));
 		y[i] = 300 + (((20*i+10)) * Math.sin((j*-theta)));
-
 	  }  
+	   x2 = 300 + 30 * Math.cos((6*-theta) + 3 * ( -Math.PI / Math.asin(.290002)));   
+	   y2 = 300 + 30 * Math.sin((6*-theta) + 3 * ( -Math.PI / Math.asin(.290002)));
+	   
+	   x3 = 300 + 110 * Math.cos((10*-theta) + 3 * ( -Math.PI / Math.asin(.10002)));
+	   y3 = 300 + 110 * Math.sin((10*-theta) + 3 * ( -Math.PI / Math.asin(.10002)));
+	   
+	   x4 = 300 + 290 * Math.cos((19*theta) + 3 * ( Math.PI / Math.asin(.030002)));
+	   y4 = 300 + 290 * Math.sin((19*theta) + 3 * ( Math.PI / Math.asin(.030002)));
+	   
 }
 
   public void draw(Graphics2D g2)
@@ -67,14 +92,38 @@ public void translate(int dx, int dy){
     }
 
     Random gen = new Random();
-    
     for (int i=0; i<= n-1; i= i+1){
       Color c = new Color(Math.abs(gen.nextInt()) % 255, Math.abs(gen.nextInt()) % 255, Math.abs(gen.nextInt()) % 255);
       g2.setColor(c);
+      if(i == 1) {
+    	  temp = c;
+      }
+      if(i == 5) {
+    	  temp2 = c;
+      }
+      if(i == 14) {
+    	  temp3 = c;
+      }
+      
       Ellipse2D.Double ball = new Ellipse2D.Double(x[i]-10, y[i]-10, 20, 20);
       g2.fill(ball);     
       g2.draw(ball);
      }
+    
+    g2.setColor(temp);
+    Ellipse2D.Double ball2 = new Ellipse2D.Double(x2-10, y2-10, 20, 20);
+    g2.fill(ball2);     
+    g2.draw(ball2);
+    
+    g2.setColor(temp2);
+    ball2 = new Ellipse2D.Double(x3-10, y3-10, 20, 20);
+    g2.fill(ball2);     
+    g2.draw(ball2);
+    
+    g2.setColor(temp3);
+    ball2 = new Ellipse2D.Double(x4-10, y4-10, 20, 20);
+    g2.fill(ball2);     
+    g2.draw(ball2);
 
   } 
 
